c25a3f495892c6086605104cf6a5f3164761cdcb
Armis <arminas.mil@gmail.com>
2020-10-23 22:46:02 +0100

    added drink class, value calculation.

--- lib/ResultsPage.dart
+++ lib/ResultsPage.dart
@@ -2,6 +2,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:moneyshare/HomePage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Drink{
+  String name;
+  double price;  //1.0 for pound
+  double amount; //ml
+  double percent; //0.5 = 50%
+  double value;   //Units per pound
+
+  Drink(this.name, this.price, this.amount,this.percent){
+    double alc = percent * amount;
+    this.value = alc/price/10;
+  }
+
+  String toString()
+  {
+    return this.name + " " + this.price.toString() +
+        " " + this.percent.toString() + " " + this.value.toString();
+  }
+}
+
+
 
 class ResultsPage extends StatefulWidget {
 
@@ -11,13 +34,15 @@ class ResultsPage extends StatefulWidget {
   }
 }
 
-class _ResultsPageState extends State<ResultsPage>{
+class _ResultsPageState extends State<ResultsPage> {
   var result;
+
   @override
   void initState() {
     super.initState();
     generateResult();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,9 +54,6 @@ class _ResultsPageState extends State<ResultsPage>{
           Text(result)
 
 
-
-
-
         ],
       ),
     );
@@ -40,11 +62,11 @@ class _ResultsPageState extends State<ResultsPage>{
   }
 
   generateResult() {
-    result =  "test";
+    result = "test";
   }
 
 
-  saveData(String pubName, List<String> list) async{
+  saveData(String pubName, List<String> list) async {
     final prefs = await SharedPreferences.getInstance();
     final key = pubName;
     final value = list;
@@ -58,11 +80,49 @@ class _ResultsPageState extends State<ResultsPage>{
     var value;
     var key;
     var i = keys.iterator;
-    while(i.moveNext())
-    {
+    while (i.moveNext()) {
       key = i.current;
       value = prefs.getStringList(key);
       print('read $value from $key');
     }
+  }
 
+}
+
+
+
+
+
+//pass a list of drinks
+void _getDrinks() async {
+  FirebaseApp firebase = await Firebase.initializeApp();
+  FirebaseFirestore _fireStoreInstance = FirebaseFirestore.instance;
+
+  var collectionReference = _fireStoreInstance.collection('Drinks');
+  List<Drink> result = [];
+
+  for(int i = 0; i < 1; i++){
+    //String name = names.elementAt(i);
+    var query = collectionReference.where("Name", isEqualTo: "Hopstach");
+
+    QuerySnapshot snap = await query.get();
+
+    if(snap.docs.isNotEmpty){
+      for(DocumentSnapshot j in snap.docs){
+        double amount = 568;
+        if(j.get("Type") == "Beer"){
+          amount = 568;
+        }
+        else if(j.get("Type") == "Liquor"){
+          amount = 50;
+        }
+        Drink drink = new Drink(j.get("Name"), 3, amount, j.get("%"));
+        result.add(drink); //need to read the price and the amount from the menu
+        print(drink.name + " " + drink.percent.toString() + " " +drink.value.toString());
+      }
+    }
+
+  }
+  Comparator<Drink> compareValue = (a,b) => a.value.compareTo(b.value);
+  result.sort(compareValue);
 }
