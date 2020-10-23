f8b1b2c3b191a051d4be95ae4c2bb346073e3c6a
Isaac-Orr <isaac.samuel.orr@gmail.com>
2020-10-23 23:59:21 +0100

    Takes armians output and displays

--- lib/ResultsPage.dart
+++ lib/ResultsPage.dart
@@ -5,25 +5,23 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Drink{
+class Drink {
   String name;
-  double price;  //1.0 for pound
+  double price; //1.0 for pound
   double amount; //ml
   double percent; //0.5 = 50%
-  double value;   //Units per pound
+  double value; //Units per pound
 
-  Drink(this.name, this.price, this.amount,this.percent){
+  Drink(this.name, this.price, this.amount, this.percent) {
     double alc = percent * amount;
-    this.value = alc/price/10;
+    this.value = alc / price / 10;
   }
 
-  String toString()
-  {
+  String toString() {
     return this.name + " " + this.price.toString() +
         " " + this.percent.toString() + " " + this.value.toString();
   }
 }
-
 
 
 class ResultsPage extends StatefulWidget {
@@ -35,36 +33,50 @@ class ResultsPage extends StatefulWidget {
 }
 
 class _ResultsPageState extends State<ResultsPage> {
-  var result;
+
 
   @override
   void initState() {
     super.initState();
-    generateResult();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Results"),
-      ),
-      body: Column(
-        children: [
-          Text(result)
+    return FutureBuilder(
+      future: _getDrinks(),
+      builder: (context, drinksList) {
+        print("test0");
+        return drinksList.connectionState == ConnectionState.done ?
+         Scaffold(
+            appBar: AppBar(
+              title: Text("Results"),
+            ),
+            body: Container(
+              child:  Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  child: ListView.builder(itemBuilder: (context, index) {
+
+                    print("test 1");
+                    final Drink drink = drinksList.data[index];
+
+                    return ListTile(
+                      title: Text(drink.name),
+                      subtitle: Text(drink.value.toString()),
+                    );
+
+                  },
+                  itemCount: drinksList.data.length,)
 
 
-        ],
-      ),
+              )
+            )
+            ): Center(child: CircularProgressIndicator(),);
+      },
     );
-    // TODO: implement build
-    throw UnimplementedError();
-  }
 
-  generateResult() {
-    result = "test";
-  }
 
+  }
 
   saveData(String pubName, List<String> list) async {
     final prefs = await SharedPreferences.getInstance();
@@ -90,39 +102,37 @@ class _ResultsPageState extends State<ResultsPage> {
 }
 
 
-
-
-
 //pass a list of drinks
-void _getDrinks() async {
+Future<List<Drink>> _getDrinks() async {
   FirebaseApp firebase = await Firebase.initializeApp();
   FirebaseFirestore _fireStoreInstance = FirebaseFirestore.instance;
 
   var collectionReference = _fireStoreInstance.collection('Drinks');
   List<Drink> result = [];
 
-  for(int i = 0; i < 1; i++){
+  for (int i = 0; i < 1; i++) {
     //String name = names.elementAt(i);
     var query = collectionReference.where("Name", isEqualTo: "Hopstach");
 
     QuerySnapshot snap = await query.get();
 
-    if(snap.docs.isNotEmpty){
-      for(DocumentSnapshot j in snap.docs){
+    if (snap.docs.isNotEmpty) {
+      for (DocumentSnapshot j in snap.docs) {
         double amount = 568;
-        if(j.get("Type") == "Beer"){
+        if (j.get("Type") == "Beer") {
           amount = 568;
         }
-        else if(j.get("Type") == "Liquor"){
+        else if (j.get("Type") == "Liquor") {
           amount = 50;
         }
         Drink drink = new Drink(j.get("Name"), 3, amount, j.get("%"));
         result.add(drink); //need to read the price and the amount from the menu
-        print(drink.name + " " + drink.percent.toString() + " " +drink.value.toString());
+        print(drink.name + " " + drink.percent.toString() + " " +
+            drink.value.toString());
       }
     }
-
   }
-  Comparator<Drink> compareValue = (a,b) => a.value.compareTo(b.value);
+  Comparator<Drink> compareValue = (a, b) => a.value.compareTo(b.value);
   result.sort(compareValue);
+  return(result);
 }
