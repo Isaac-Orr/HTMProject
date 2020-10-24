77618b2b0d023f9cba3c10d0a01e18cfbd284c1f
ewand <ewandobres@gmail.com>
2020-10-24 15:34:17 +0100

    Added image preview screen

--- lib/ResultsPage.dart
+++ lib/ResultsPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:moneyshare/HomePage.dart';
+import 'package:quiver/collection.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
